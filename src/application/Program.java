8d7a6f869c8fc469f371bce88ffd282bb23a60fd
FernandoJMartins <julio8martins@gmail.com>
2024-03-20 01:46:30 -0300

    FactoryDao and DepDao and DaoFac

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,5 +17,8 @@ public class Program {
 		
 		Seller s = new Seller(1,"Pablo", "p@gmail.com", new Date(), 3000.0, d);
 		System.out.println(s);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 	}
 }
