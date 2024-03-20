a0e85296f8232895f282dbcc693e7bb96e85025e
FernandoJMartins <julio8martins@gmail.com>
2024-03-20 01:25:51 -0300

    seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d = new Department(1, "Books");
-		
 		System.out.println(d);
 
+		
+		Seller s = new Seller(1,"Pablo", "p@gmail.com", new Date(), 3000.0, d);
+		System.out.println(s);
 	}
-
 }
